8e7313b2799913b244af1a3c6061791451b7b075
Swarnima2004 <swarnima3108@gmail.com>
2025-04-22 21:49:36 +0530

    udated dashboard

---
+++ app/src/main/java/com/example/studybuddy/util/Common.kt
@@ -0,0 +1,19 @@
+package com.example.studybuddy.util
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import com.example.studybuddy.ui.theme.Green
+
+enum class Priority(val title: String, val color: Color, val value: Int) {
+    LOW(title = "Low", color = Green, value = 0),
+
+    MEDIUM(title = "Medium", color = Yellow, value = 1),
+
+    HIGH(title = "High", color = Color.Red, value = 2);
+
+    companion object{
+        fun fromInt(value: Int) = values().firstOrNull{ it.value == value }?: MEDIUM
+
+    }
+
+}
